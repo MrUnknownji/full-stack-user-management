0168db3ad891b445cc56d863861e99e9530fedf0
Sandeep Kumar <sandeepkhati788@gmail.com>
2025-05-25 11:11:35 +0530

    Compile error solved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import dotenv from "dotenv";
 
 dotenv.config();
-const API_URL = process.env.API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 const USERS_PER_PAGE = 5;
 
 function App() {
